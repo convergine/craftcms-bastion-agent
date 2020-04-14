64cdf6d5d856b0e1aca417be4cf4ab3fde024687
denisus <denis@convergine.com>
2020-04-14 15:44:38 +0300

    Release 1.0.2

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,7 @@ namespace criticalgears\bastionagent;
 
 /*
  * Bastion Plugin for CraftCMS
- * Version 1.0.1
+ * Version 1.0.2
  */
 
 use craft\ckeditor\Field;
@@ -15,7 +15,7 @@ use yii\base\Event;
 
 class Plugin extends  \craft\base\Plugin {
 
-    const VERSION = '1.0.1';
+    const VERSION = '1.0.2';
 
     public $hasCpSettings=true;
 
