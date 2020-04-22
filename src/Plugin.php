ab357280ded3a62c11d4e20945b175d182b3b938
denisus <denis@convergine.com>
2020-04-22 13:19:10 +0300

    Hash tokens

--- src/Plugin.php
+++ src/Plugin.php
@@ -3,7 +3,70 @@ namespace criticalgears\bastionagent;
 
 /*
  * Bastion Plugin for CraftCMS
+ * Version 1.0.6
  */
+
+use craft\ckeditor\Field;
+use craft\events\ModelEvent;
+use criticalgears\bastionagent\models\SettingsModel;
+use criticalgears\bastionagent\services\AgentService;
+use yii\base\Event;
+
 class Plugin extends  \craft\base\Plugin {
-    public $hasCpSection=true;
+
+    const VERSION = '1.0.6';
+
+    public $hasCpSettings=true;
+
+    public function init()
+    {
+        /** @var SettingsModel $settings */
+        $settings = $this->getSettings();
+
+        // Validate token before save settings
+
+        Event::on(Plugin::class, Plugin::EVENT_BEFORE_SAVE_SETTINGS, function (ModelEvent $e) {
+
+            $request = \Craft::$app->request->post();
+            if(!isset($request['settings']['secretToken']))
+                return;
+
+            if($request['settings']['secretToken'] !== $e->data) {
+                $response = (new AgentService())->sendValidateRequest($request['settings']['secretToken']);
+                if ($response !== false && isset($response->status) && $response->status) {
+                    $e->isValid = true;
+                } else {
+                    $e->isValid = false;
+                    \Craft::$app->getSession()->setNotice('Incorrect token');
+                }
+            }
+        },$settings->secretToken);
+
+        Event::on(Plugin::class, Plugin::EVENT_AFTER_SAVE_SETTINGS, function () {
+            $request = \Craft::$app->request->post();
+            if(!isset($request['settings']['secretToken']))
+                return;
+            (new AgentService())->sendSettingsRequest();
+
+        });
+    }
+
+    protected function createSettingsModel()
+    {
+        return new SettingsModel();
+    }
+
+    protected function settingsHtml()
+    {
+        return \Craft::$app->getView()->renderTemplate('bastion-agent/_settings',[
+            'settings'=>$this->getSettings()
+        ]);
+    }
+
+    protected function afterInstall()
+    {
+        parent::afterInstall();
+
+        (new AgentService())->sendInitialRequest($this);
+    }
 }
---
+++ src/controllers/AgentController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace criticalgears\bastionagent\controllers;
+
+use craft\helpers\App;
+use craft\mail\Mailer;
+use craft\services\ProjectConfig;
+use craft\services\Updates;
+use craft\web\Controller;
+use criticalgears\bastionagent\models\SettingsModel;
+use criticalgears\bastionagent\Plugin;
+use criticalgears\bastionagent\services\AgentService;
+use Imagine\Gd\Imagine;
+use Twig\Environment;
+use Yii;
+use yii\web\BadRequestHttpException;
+use yii\web\UnauthorizedHttpException;
+
+class AgentController extends Controller
+{
+    protected $allowAnonymous = ['heartbeat'];
+
+    public $enableCsrfValidation = false;
+    // Public Methods
+    // =========================================================================
+
+    /*
+     *
+     */
+    public function actionHeartbeat()
+    {
+
+        $plugin = Plugin::getInstance();
+        /** @var SettingsModel $settings */
+        $settings = $plugin->getSettings();
+
+        $headers = \Craft::$app->request->headers;
+
+        if( !isset($headers['Token']) ) {
+            $message = 'Missing parameters';
+
+            throw new BadRequestHttpException($message);
+        }
+
+        //if( $headers['Token'] != $settings->secretToken ) {
+        if( $headers['Token'] != hash('sha256', $settings->secretToken) ) {
+            $message = 'Unauthenticated access is not allowed.';
+
+            throw new UnauthorizedHttpException($message);
+        }
+
+        $apiURL = 'https://citadel.bastion.cloud/api/v1/websites/craftcms/heartbeat';
+        $sendData = [];
+
+        if(/*time() - $settings->lastBeatTime >= $settings->telemetryInterval*/1){
+
+            /*
+             * All plugins info
+             */
+            $sendData['plugins'] = \Craft::$app->plugins->getAllPluginInfo();
+
+            /*
+             * Get latest CraftCMS version
+             */
+            $updates = \Craft::$app->updates->getUpdates();
+            $lastVersion = $updates->cms->releases[0]->version;
+
+            /*
+             * CraftCMS site info
+             */
+            $sendData['craftcms'] = [
+                'version' => \Craft::$app->getVersion(),
+                'latest_version' => $lastVersion,
+                'home_url' => (new AgentService())->getSiteURL()
+            ];
+
+            /*
+             * Server info
+             */
+            $sendData["server"] = [
+                'web_server' => $_SERVER["SERVER_SOFTWARE"],
+                'protocol' => $_SERVER["SERVER_PROTOCOL"],
+                'gw_interface' => $_SERVER["GATEWAY_INTERFACE"],
+                'http_accept_encoding' => $_SERVER["HTTP_ACCEPT_ENCODING"],
+            ];
+
+            /*
+             * Server info
+             */
+            $sendData["system"] = [
+                'phpVersion' => App::phpVersion(),
+                'osVersion' => PHP_OS . ' ' . php_uname('r'),
+                'yiiVersion' => Yii::getVersion(),
+                'twigVersion' => Environment::VERSION,
+                'imagineVersion' => Imagine::VERSION
+            ];
+
+
+            $response = (new AgentService())->sendAPI($apiURL,$sendData);
+
+            if($response!==false){
+                //\Craft::dump($response);
+                \Craft::$app->plugins->savePluginSettings($plugin,['lastBeatTime' => time()]);
+            }
+
+        }else{
+            $response = [
+                'status' => 0,
+                'message' => 'Heartbeat was recently run.'
+            ];
+        }
+
+        return $this->asJson($response);
+    }
+
+
+}
